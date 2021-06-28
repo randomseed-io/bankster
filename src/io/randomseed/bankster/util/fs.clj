1a649f4f47cc84f7c293bda3cad3675358d5c69f
Paweł Wilk <siefca@gnu.org>
2021-06-28 15:11:02 +0200

    fs/relative-path? and fs/absolute-path? now rely on java.nio.file.Paths

--- src/io/randomseed/bankster/util/fs.clj
+++ src/io/randomseed/bankster/util/fs.clj
@@ -12,25 +12,24 @@
             [io.randomseed.bankster.util :refer  :all])
 
   (:import [org.apache.commons.io.input BOMInputStream]
-           [org.apache.commons.io        ByteOrderMark]))
+           [org.apache.commons.io        ByteOrderMark]
+           [java.nio.file                        Paths]))
 
 (def ^String ^private ^const default-encoding
   "Default encoding for input files."
   "UTF-8")
 
-(defn ^Boolean relative-path?
-  "Returns true if the given path is relative. False otherwise."
-  [^String pathname]
-  (try (io/as-relative-path pathname)
-       (catch IllegalArgumentException e
-         false))
-  true)
+(def ^"[Ljava.lang.String;"
+  empty-str-ary
+  (into-array [""]))
 
-(def
-  ^{:tag Boolean :arglists '([^String pathname])}
-  absolute-path?
-  "Returns true if the given path is absolute. False otherwise."
-  (complement relative-path?))
+(defn ^Boolean absolute-path?
+  [pathname]
+  (.isAbsolute ^Paths (Paths/get ^String (str pathname)
+                                 ^"[Ljava.lang.String;" empty-str-ary)))
+
+(def ^Boolean relative-path?
+  (complement absolute-path?))
 
 (defn ^clojure.lang.LazySeq get-java-classpath-folders
   "Lists all directories which exist in Java classpath as a sequence of
